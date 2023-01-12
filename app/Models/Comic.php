793070799db03a58d9c87b0e1a89f652c859b6bc
DenisConsta <denisconstantin338@gmail.com>
2023-01-12 15:39:19 +0100

    [init] first migration + ComicsTableSeeder

---
+++ app/Models/Comic.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Comic extends Model
+{
+    use HasFactory;
+
+    public static function generateSlug($string){
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+        $c = 1;
+        $house_exist = Comic::where('slug', $slug)->first();
+        while($house_exist){
+            $slug = $original_slug . '-' . $c;
+            $house_exist = Comic::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
+}
